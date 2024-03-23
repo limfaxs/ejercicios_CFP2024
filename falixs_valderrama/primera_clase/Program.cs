943b8d234a6c9961f89e0d330d92f31966c26e30
limfaxs <falixs62@gmail.com>
2024-03-23 07:03:20 -0300

    mi primer ejercicio

---
+++ falixs_valderrama/primera_clase/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace primera_clase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            string edadTexto;
+            int edadNumerica;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su apellido: ");
+            apellido = Console.ReadLine();
+
+            Console.Write("Ingrese su edad: ");
+            edadTexto = Console.ReadLine();
+
+            edadNumerica = int.Parse(edadTexto);
+
+            //Console.WriteLine("Bienvenido " + nombre + " " + apellido + ", usted tiene: " + edadNumerica + " años.");
+
+
+            //Console.WriteLine("{1} Bienvenido {2} {0}, ud tiene: {1} años. ", apellido, edadNumerica, nombre);
+
+            Console.WriteLine($"Bienvenido {nombre} {apellido}, ud tiene: {edadNumerica} años.");
+
+        }
+        
+    }
+    }
