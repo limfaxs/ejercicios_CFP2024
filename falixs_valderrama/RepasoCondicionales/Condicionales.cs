2994a31cecac0fe95230d24e985c07b80498f3b5
limfaxs <falixs62@gmail.com>
2024-03-25 15:03:32 -0300

    Repaso Condicionales

---
+++ falixs_valderrama/RepasoCondicionales/Condicionales.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace RepasoCondicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Al ingresar una edad menor a 18 años y un estado civil distinto a soltero, 
+            //mostrar el mensaje: “Es muy pequeño para NO ser soltero”
+
+
+            int edad;
+            string edadTexto;
+            string estadoCivil;
+
+            Console.Write("Ingrese su edad: ");
+            edadTexto = Console.ReadLine();
+
+            edad = int.Parse(edadTexto);
+
+
+            Console.Write("Ingrese su estado Civil: ");
+
+            estadoCivil = Console.ReadLine();
+
+            if (edad < 18 && estadoCivil == "casado")
+            {
+                Console.WriteLine("Es muy pequeño para no ser soltero");
+            }
+
+        }
+    }
+}
+
--- falixs_valderrama/RepasoCondicionales/obj/Debug/net8.0/RepasoCondicionales.AssemblyInfo.cs
+++ falixs_valderrama/RepasoCondicionales/obj/Debug/net8.0/RepasoCondicionales.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("primera_clase")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("RepasoCondicionales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+943b8d234a6c9961f89e0d330d92f31966c26e30")]
-[assembly: System.Reflection.AssemblyProductAttribute("primera_clase")]
-[assembly: System.Reflection.AssemblyTitleAttribute("primera_clase")]
+[assembly: System.Reflection.AssemblyProductAttribute("RepasoCondicionales")]
+[assembly: System.Reflection.AssemblyTitleAttribute("RepasoCondicionales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
